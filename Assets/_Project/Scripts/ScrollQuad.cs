79d27f8f81b6b3be9b4ac9c8d77b617bdbf70506
Gustavo Silva <xgustavossilva@gmail.com>
2019-07-08 22:32:50 -0300

    [Add] Scrolling Objects

---
+++ Assets/_Project/Scripts/ScrollQuad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class ScrollQuad : MonoBehaviour {
+
+    public float speed;
+    private Material quadMaterial;
+    private Vector2 offset;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        quadMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(quadMaterial.mainTextureOffset.x, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset.x += Time.deltaTime * speed;
+        offset.x %= 1;
+        quadMaterial.mainTextureOffset = offset;
+    }
+}
---
+++ Assets/_Project/Scripts/ScrollingObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 1f;
+    public float initialPos;
+    public float finalPos;
+    public float yMax;
+    public float yMin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * (speed * Time.deltaTime);
+        if(transform.position.x < finalPos) {
+            transform.position = new Vector2(initialPos, Random.Range(yMin, yMax));
+        }
+    }
+}
