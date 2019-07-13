5b35169c7773d209628df08a39e2f65cdb9cc672
Gustavo Silva <xgustavossilva@gmail.com>
2019-07-13 00:30:35 -0300

    [Update] Fix player movement

--- Assets/_Project/Scripts/PlayerControl.cs
+++ Assets/_Project/Scripts/PlayerControl.cs
@@ -7,8 +7,8 @@ public class PlayerControl : MonoBehaviour
     public Animator playerAnimator;
 
     public bool isJumping = false;
-    public bool isWalking = false;
     public bool isCrounching = false;
+    [SerializeField] private bool isLanding = true;
 
     public Collider2D initialBodyCollider;
     public Collider2D initialNoseCollider;
@@ -19,15 +19,17 @@ public class PlayerControl : MonoBehaviour
 
     public AudioSource dinoSounds;
     public AudioClip jumpSound;
-    public float force;
-    [SerializeField] private bool isLanding = true;
     private Rigidbody2D playerBody;
+    public float force;
 
     private void Awake() {
         playerBody = gameObject.GetComponent<Rigidbody2D>();
     }
-    public bool CanJump() {
+    public bool TryToJump() {
         bool jumpClicked = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow);
+        if (jumpClicked) {
+            isJumping = true;
+        }
         return jumpClicked && isLanding && !isCrounching;
     }
 
@@ -39,40 +41,54 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.isLanding = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundMask);
-
         if(!GameManager.instance.hasStarted && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))) {
+            isJumping = true;
             //Diferete do Javascript, o C# entende '' como um caractere e "" como uma string, então cuidado...
             // playerAnimator.SetBool("isJumping", true);
             Jump();
             GameManager.instance.hasStarted = true;
+            return;
         }
-        if(GameManager.instance.hasStarted) {
-            if(CanJump()) {
-                playerAnimator.SetBool("isWalking", false);
-                playerAnimator.SetBool("isJumping", true);
-                Jump();
-            }
-            if(Input.GetKeyDown(KeyCode.DownArrow) && isLanding) {
-                this.isCrounching = true;
-                this.initialBodyCollider.enabled = false;
-                this.initialNoseCollider.enabled = false;
-                this.crounchCollider.enabled = true;
-                playerAnimator.SetBool("isWalking", false);
-                playerAnimator.SetBool("isCrounching", true);
+        if (!GameManager.instance.hasStarted) {
+            return;
+        }
+        //Checa se o jogador está no chão
+        this.isLanding = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundMask);
+        //Acontece quando o jogador tenta pular e pode pular.
+        if(TryToJump()) {
+            playerAnimator.SetBool("isWalking", false);
+            playerAnimator.SetBool("isCrounching", false);
+            playerAnimator.SetBool("isJumping", true);
+            isLanding = false;
+            Jump();
+        }
+        //Acontece caso o jogador pressione a tecla, está no chão e não está pulando
+        else if(Input.GetKey(KeyCode.DownArrow) && isLanding && !isJumping) {
+            this.isCrounching = true;
+            this.initialBodyCollider.enabled = false;
+            this.initialNoseCollider.enabled = false;
+            this.crounchCollider.enabled = true;
+            playerAnimator.SetBool("isWalking", false);
+            playerAnimator.SetBool("isCrounching", true);
 
-            }
-            if(Input.GetKeyUp(KeyCode.DownArrow) && isCrounching) {
-                this.initialBodyCollider.enabled = true;
-                this.initialNoseCollider.enabled = true;
-                this.crounchCollider.enabled = false;
-                this.isCrounching = false;
-            }
-            if(isLanding && !isCrounching){
-                playerAnimator.SetBool("isWalking", true);
-                this.playerAnimator.SetBool("isCrounching", false);
-                this.playerAnimator.SetBool("isJumping", false);
-            }
         }
+        //Acontece quando o player solta a tecla para baixo e está abaixo.
+        else if(Input.GetKeyUp(KeyCode.DownArrow) && isCrounching) {
+            this.initialBodyCollider.enabled = true;
+            this.initialNoseCollider.enabled = true;
+            this.crounchCollider.enabled = false;
+            this.isCrounching = false;
+        }
+        //Está pulando e encostou no chão
+        else if(isLanding && isJumping) {
+            isJumping = false;
+        }
+        //Está no chão e 
+        else if(isLanding && !isJumping && !isCrounching){
+            playerAnimator.SetBool("isWalking", true);
+            this.playerAnimator.SetBool("isCrounching", false);
+            this.playerAnimator.SetBool("isJumping", false);
+        }
+
     }
 }
